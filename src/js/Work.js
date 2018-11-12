61f52dbc2970ae43f9a64f1ba66152360e3a0c8f
Aaron <aaron_waterhouse@yahoo.com>
2018-11-11 20:14:52 -0800

    Project component work

--- src/js/Work.js
+++ src/js/Work.js
@@ -1,38 +1,35 @@
 import React, { Component } from 'react';
 import projects from './data.js';
+import Project from './Project'
 import '../css/Project.css'; 
 
 class Work extends Component {
+
+  renderProjects(projects) {
+    return projects.map(project => {
+      return (
+        <div key={project.name}>
+          <Project 
+            name={project.name} 
+            description={project.description} 
+            url={project.url} 
+            highlightUrl={project.highlightUrl}
+          />
+        </div>
+      );
+    });
+  }
+
   render() {
-    console.log('projects: ', projects);
     return (
       <div id="work" className="inner-page-container">
         <div className="page-header">
           <h1 className="page-header-label">WORK</h1>
           <div className="page-header-divider"></div>
         </div>
-        <div className="work-page-content">
-
-          { /* Replace with components */ }
-          <div className="project-container">
-            <div className="project-img">
-              <img className="project-highlight" src="/assets/projects/propagator.png" alt="propagator-highlight" height="auto"></img>
-            </div>
-            <div className="project-meta">
-              <div className="project-name">
-                Propagator
-              </div>
-              <div className="project-description">
-                <p>A handmade zine created by a network of peers working across various creative disciplines looking to expand their art, design, and research practices.</p>
-              </div>
-              <div className="project-tags">
-                
-              </div>
-            </div>
-          </div>
-
+      
+        {this.renderProjects(projects)}     
         
-        </div>
       </div>
     );
   }
--- src/js/data.js
+++ src/js/data.js
@@ -3,27 +3,27 @@ module.exports = [
     "name" : "Propagator",
     "description" : "A handmade zine created by a network of peers working across various creative disciplines looking to expand their art, design, and research practices.",
     "url": "http://propagator.xyz",
-    "highlight-url" : "/assets/projects/propagator.png"
+    "highlightUrl" : "/assets/projects/propagator.png"
   },
 
   {
     "name" : "Perennial",
     "description": "A meditation born from daydreams, pieced together from fragments of self-reflection & self-care. A little world, a gentle vessel, a patient portal.",
     "url": "http://tbd.com",
-    "highlight-url" : "/assets/projects/perennial.png"
+    "highlightUrl" : "/assets/projects/perennial.png"
   },
 
   {
     "name" : "tenderbloom",
     "description": "An ongoing series of little renders created in the process of learning 3D modeling, animation, and working with 3D on the web.",
     "url": "http://instagram.com/tenderbloom",
-    "highlight-url" : "/assets/projects/tenderbloom.png"
+    "highlightUrl" : "/assets/projects/tenderbloom.png"
   },
 
   {
     "name" : "galaxy",
     "description": "An audiovisualization made with Processing & the Minim library.",
     "url": "http://tbd.com",
-    "highlight-url": "/assets/projects/galaxy.png"
+    "highlightUrl": "/assets/projects/galaxy.png"
   }
 ];
