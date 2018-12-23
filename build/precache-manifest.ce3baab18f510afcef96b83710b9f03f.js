4bfd3e35a763efc8f34d6a68f4799b8e1e2e7c37
rose-water <rachel.ulgado@gmail.com>
2018-12-23 00:35:31 -0800

    build

--- build/precache-manifest.ce3baab18f510afcef96b83710b9f03f.js
+++ build/precache-manifest.ce3baab18f510afcef96b83710b9f03f.js
@@ -1,186 +1,186 @@
 self.__precacheManifest = [
   {
     "revision": "c412508010088b2c123ecd0d7702cf85",
-    "url": "./static/media/pocktvr.c4125080.png"
+    "url": "/static/media/pocktvr.c4125080.png"
   },
   {
     "revision": "3f60d38d1d3b1d32dff1",
-    "url": "./static/css/main.e11bd9d9.chunk.css"
+    "url": "/static/css/main.e11bd9d9.chunk.css"
   },
   {
     "revision": "64296779f7be9af1fbfe",
-    "url": "./static/js/1.64296779.chunk.js"
+    "url": "/static/js/1.64296779.chunk.js"
   },
   {
-    "revision": "4a686d48d5a089750c49",
-    "url": "./static/js/runtime~main.4a686d48.js"
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "96ad0a0b05359707910d6a00a3ca5b91",
-    "url": "./static/media/chinafile.96ad0a0b.png"
+    "url": "/static/media/chinafile.96ad0a0b.png"
   },
   {
     "revision": "11ef9120014d8651f09613d1c01d9719",
-    "url": "./static/media/chinafile_01.11ef9120.png"
+    "url": "/static/media/chinafile_01.11ef9120.png"
   },
   {
     "revision": "efc7a8e334db2c7a36ccde656c0fc07d",
-    "url": "./static/media/chinafile_02.efc7a8e3.png"
+    "url": "/static/media/chinafile_02.efc7a8e3.png"
   },
   {
     "revision": "7e0db5db680470a631779d58866458f9",
-    "url": "./static/media/chinafile_03.7e0db5db.png"
+    "url": "/static/media/chinafile_03.7e0db5db.png"
   },
   {
     "revision": "4b889d35e6de90b6147d7471a7533b11",
-    "url": "./static/media/chinafile_04.4b889d35.png"
+    "url": "/static/media/chinafile_04.4b889d35.png"
   },
   {
     "revision": "0a27bc9c2655c8508bf1ac07b2db4c1e",
-    "url": "./static/media/chinafile_06.0a27bc9c.png"
+    "url": "/static/media/chinafile_06.0a27bc9c.png"
   },
   {
     "revision": "db1d6f19f65c5f4b35860d8f293e4b98",
-    "url": "./static/media/chordal.db1d6f19.png"
+    "url": "/static/media/chordal.db1d6f19.png"
   },
   {
     "revision": "9791bf8ad5eb98ba24c201cebd51d986",
-    "url": "./static/media/feminist-hackers.9791bf8a.png"
+    "url": "/static/media/feminist-hackers.9791bf8a.png"
   },
   {
     "revision": "d38163af4ad44b78763447dd115923e0",
-    "url": "./static/media/galaxy.d38163af.png"
+    "url": "/static/media/galaxy.d38163af.png"
   },
   {
     "revision": "e44cac677ec76ae0a295579baace451a",
-    "url": "./static/media/guise.e44cac67.png"
+    "url": "/static/media/guise.e44cac67.png"
   },
   {
     "revision": "eb7c8501d0527edca52ce77097685fb4",
-    "url": "./static/media/memoryobservatory.eb7c8501.png"
+    "url": "/static/media/memoryobservatory.eb7c8501.png"
   },
   {
     "revision": "1e060248121e64151bd35037ffae0fbb",
-    "url": "./static/media/memoryobservatory_1.1e060248.png"
+    "url": "/static/media/memoryobservatory_1.1e060248.png"
   },
   {
     "revision": "113e55fd0d2c2cfb1b2d520f697dd671",
-    "url": "./static/media/memoryobservatory_2.113e55fd.png"
+    "url": "/static/media/memoryobservatory_2.113e55fd.png"
   },
   {
     "revision": "b7488ced54885d1970273ba5c39a5924",
-    "url": "./static/media/memoryobservatory_3.b7488ced.png"
+    "url": "/static/media/memoryobservatory_3.b7488ced.png"
   },
   {
     "revision": "7141e902b9012bff700138b97cfbe3aa",
-    "url": "./static/media/memoryobservatory_4.7141e902.png"
+    "url": "/static/media/memoryobservatory_4.7141e902.png"
   },
   {
     "revision": "6e659e8a699455f36c9296d4d716d988",
-    "url": "./static/media/paracosm.6e659e8a.png"
+    "url": "/static/media/paracosm.6e659e8a.png"
   },
   {
     "revision": "be76411c817d8ebc89150fe55051249d",
-    "url": "./static/media/perennial.be76411c.png"
+    "url": "/static/media/perennial.be76411c.png"
   },
   {
     "revision": "47dd38e7ce03574812f53a6f73933c58",
-    "url": "./static/media/install.47dd38e7.png"
+    "url": "/static/media/install.47dd38e7.png"
   },
   {
     "revision": "b7dc7c7a77238960c8071a0ea4b069f8",
-    "url": "./static/media/ss_1.b7dc7c7a.png"
+    "url": "/static/media/ss_1.b7dc7c7a.png"
   },
   {
     "revision": "3f60d38d1d3b1d32dff1",
-    "url": "./static/js/main.3f60d38d.chunk.js"
+    "url": "/static/js/main.3f60d38d.chunk.js"
   },
   {
     "revision": "f6c1cfa7af052bf5fdc6235b049329f8",
-    "url": "./static/media/pocktvr-1.f6c1cfa7.png"
+    "url": "/static/media/pocktvr-1.f6c1cfa7.png"
   },
   {
     "revision": "79ce926449daf6d370875d99a4ac12c1",
-    "url": "./static/media/pocktvr-2.79ce9264.png"
+    "url": "/static/media/pocktvr-2.79ce9264.png"
   },
   {
     "revision": "f0b236c675b2c3fed34d138397b1e9a2",
-    "url": "./static/media/propagator.f0b236c6.png"
+    "url": "/static/media/propagator.f0b236c6.png"
   },
   {
     "revision": "18635e7ccc7e5d02ae52f75427ce362a",
-    "url": "./static/media/shrines.18635e7c.png"
+    "url": "/static/media/shrines.18635e7c.png"
   },
   {
     "revision": "a8b7a5d012b33b895638e801cd07a188",
-    "url": "./static/media/tenderbloom.a8b7a5d0.png"
+    "url": "/static/media/tenderbloom.a8b7a5d0.png"
   },
   {
     "revision": "9697eeec296f67a465fb401ea63abcd5",
-    "url": "./static/media/aloe.9697eeec.png"
+    "url": "/static/media/aloe.9697eeec.png"
   },
   {
     "revision": "95d23581c22101e03d709bfc49deb4a9",
-    "url": "./static/media/monstera_still.95d23581.png"
+    "url": "/static/media/monstera_still.95d23581.png"
   },
   {
     "revision": "1b29f44c7bbc01dc8dcbd8c302d834af",
-    "url": "./static/media/sketch-1.1b29f44c.png"
+    "url": "/static/media/sketch-1.1b29f44c.png"
   },
   {
     "revision": "33bbd543267a208889f883d03677e374",
-    "url": "./static/media/vidcoach.33bbd543.png"
+    "url": "/static/media/vidcoach.33bbd543.png"
   },
   {
     "revision": "ff088890d9f7cebe17723d626538cd08",
-    "url": "./static/media/vidcoach.ff088890.png"
+    "url": "/static/media/vidcoach.ff088890.png"
   },
   {
     "revision": "f00fcee143063588b65044d4a3c92084",
-    "url": "./static/media/rachel-rose-ulgado-2018-CV.f00fcee1.pdf"
+    "url": "/static/media/rachel-rose-ulgado-2018-CV.f00fcee1.pdf"
   },
   {
     "revision": "5a9ecb0f8bdb5dbd38b02072c0445446",
-    "url": "./static/media/arrow.5a9ecb0f.svg"
+    "url": "/static/media/arrow.5a9ecb0f.svg"
   },
   {
     "revision": "bd084393d93971840d5b227c72a4b164",
-    "url": "./static/media/logo-instagram.bd084393.svg"
+    "url": "/static/media/logo-instagram.bd084393.svg"
   },
   {
     "revision": "8dd4450bdd8fbda3a0c0ecd527c3ac3c",
-    "url": "./static/media/logo-email.8dd4450b.svg"
+    "url": "/static/media/logo-email.8dd4450b.svg"
   },
   {
     "revision": "a1fe4f0f29fa51d07d51cb917239fa65",
-    "url": "./static/media/feminist-hackers-1.a1fe4f0f.jpg"
+    "url": "/static/media/feminist-hackers-1.a1fe4f0f.jpg"
   },
   {
     "revision": "8ec88d3dc651b128a06631239a0cc5b8",
-    "url": "./static/media/feminist-hackers-2.8ec88d3d.jpg"
+    "url": "/static/media/feminist-hackers-2.8ec88d3d.jpg"
   },
   {
     "revision": "3ce687cc048ec81b58874db66b57f9ac",
-    "url": "./static/media/feminist-hackers-3.3ce687cc.jpg"
+    "url": "/static/media/feminist-hackers-3.3ce687cc.jpg"
   },
   {
     "revision": "27a67ec8dd92ddae5a4aa839d53e0f72",
-    "url": "./static/media/feminist-hackers-4.27a67ec8.jpg"
+    "url": "/static/media/feminist-hackers-4.27a67ec8.jpg"
   },
   {
     "revision": "7f3d2c89b86363ddf8751b467b145b2a",
-    "url": "./static/media/feminist-hackers-5.7f3d2c89.jpg"
+    "url": "/static/media/feminist-hackers-5.7f3d2c89.jpg"
   },
   {
     "revision": "0429989b3a6719ed0b91702897de5f52",
-    "url": "./static/media/vidcoach.0429989b.jpg"
+    "url": "/static/media/vidcoach.0429989b.jpg"
   },
   {
     "revision": "69d5c72bc783ef5b354f9a43282e8225",
-    "url": "./static/media/shortened.69d5c72b.gif"
+    "url": "/static/media/shortened.69d5c72b.gif"
   },
   {
-    "revision": "d48b5691b45d6be88f1a99ec7837e3c6",
-    "url": "./index.html"
+    "revision": "5e9332093047f287e093d279b9ebb8c1",
+    "url": "/index.html"
   }
 ];
