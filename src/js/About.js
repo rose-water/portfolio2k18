5cd08af85c265b88a5af87a959a60f9f6568359d
rose-water <rachel.ulgado@gmail.com>
2019-08-06 12:25:30 -0700

    about updates

--- src/js/About.js
+++ src/js/About.js
@@ -9,17 +9,15 @@ class About extends Component {
         <Nav />
         <div id="about" className="inner-page-container">
           <div className="page-content about">
-            <p>I’m an artist, technologist, and researcher working with code as a medium for exploring new ways of interacting and engaging with the world. My most recent work is focused on creating 3D interactive experiences for VR and the web.</p>
+            <p>I’m an artist / designer / technologist working with code as a medium for exploring new ways of interacting and engaging with the world. My most recent work is focused on creating 3D interactive experiences for VR and the web.</p>
 
-            <p>I am a currently a resident of the incubator program at Gray Area Foundation for the Arts, where I also teach and mentor students in creative coding and media arts.</p>
+            <p>I am currently pursuing my MFA in Media Design Practices at Art Center College of Design.</p>
 
-            <p>My bachelor’s degrees are in anthropology and informatics (specialization in human-computer interaction) from UC Irvine, where I started a campus makerspace and designed educational tools for adolescents with Autism Spectrum Disorder.</p>
+            <p>I hold an M.S. in Human-Centered Design and Engineering from the University of Washington, where I spent my time researching the sociocultural aspects of hacker and maker culture, particularly around feminist hackerspace communities. My bachelor’s degrees are in anthropology and informatics (specialization in human-computer interaction) from UC Irvine.</p>
 
-            <p>I hold a master’s degree is in human-centered design and engineering from the University of Washington, where I spent my time researching the sociocultural aspects of hacker and maker culture, particularly around feminist hackerspace communities.</p>
-
-            <p>I was a student at the School for Poetic Computation in New York in 2014.</p>
+            <p>Previously, I was a resident artist at the Gray Area Foundation for the Arts incubator program. During my time there, I also taught web development and creative coding courses in the 10-week immersive program. I was a student at the School for Poetic Computation in New York in 2014.</p>
             
-            <p>My past work experience has involved a hybrid of UX research and web development at several tech and design companies, including IBM, Intel, Schema Design, and Xerox PARC.</p>
+            <p>My professional experience has involved a hybrid of UX research and web development at several tech and design companies, including IBM, Intel, Schema Design, and Xerox PARC.</p>
           </div>
         </div>
       </div>
